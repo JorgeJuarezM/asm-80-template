f7e8667734eb2d47f5f9d655eb402c91c459ebaf
Jorge Juarez <contacto@jorgejuarez.net>
2025-03-30 10:16:50 -0600

    feat: complete BDos example

--- src/main.asm
+++ src/main.asm
@@ -1,21 +1,36 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;;  XPire - Intel 8080 Development Framework
+;;  XPire - Intel 8080 Development Framework.
+;;
+;;  BDos Printing Example.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;
-.area _DATA
-.area _CODE
+.area _DATA                 ; Declare DATA Area.
+.area _CODE                 ; Declare CODE Area.
 ;;
+JMP     _MAIN               ; Jump to Main Procedure.
+;;
+;;  Declare Global Symbols
 ;;
 .globl  print_string
+.globl  print_char
 ;;
 ;;
 _MAIN:
-    LXI     D, string
+    LXI     D, string       ; DE = Pointer to string.
     CALL    print_string
-    JMP     .
 
-
-string: .ascii  "XPire => Hello World$"
+    MVI     A, 10           ; Number of characters to print.
+    MVI     E, "."          ; Character to print.
+print_char_loop:
+    CALL    print_char
+    DCR     A
+    JNZ     print_char_loop
+    RET                     ; Finish program execution.
+;;
+;;  Define string to print.
+;;
+string: .ascii  "XPire => Hello World"
+        .DB     "$"
